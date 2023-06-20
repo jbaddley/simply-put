2c8acbca654683f0761de1d8196b7638928c55b6
Jason Baddley <jasonbaddley@gmail.com>
2023-06-20 10:41:21 -0600

    deploying

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <footer className='bg-stone-300 py-4 text-center text-white'>
       <div className='container'>
-        <p className='text-sm uppercase'>NextAuth Demo in NextJS 13</p>
+        <p className='text-sm uppercase'>Simply Put - Health, Wealth, Music, Lifestyle</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
