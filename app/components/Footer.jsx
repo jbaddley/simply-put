217d061d9928341f1d5255238f3c649462aa805e
Jason Baddley <jasonbaddley@gmail.com>
2023-06-20 10:55:09 -0600

    deploying

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className='bg-stone-300 py-4 text-center text-white'>
       <div className='container'>
-        <p className='text-sm uppercase'>Simply Put - Health, Wealth, Music, Lifestyle</p>
+        <p className='text-sm uppercase'>Simply Put - Health, Wealth and Music</p>
       </div>
     </footer>
   );
