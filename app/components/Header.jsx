7edf4a8ffedd9e0b849f8cd0f1f6840d6a5b7f0e
Jason Baddley <jasonbaddley@gmail.com>
2023-06-20 10:27:55 -0600

    deploying

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,14 @@
-import Link from 'next/link'
-import SignInButton from './SignInButton'
+import Link from "next/link";
+import SignInButton from "./SignInButton";
+import Image from "next/image";
+import logo from "../assets/SP.svg";
 
 const Header = () => {
   return (
     <header className='flex h-24 flex-col justify-center bg-stone-100'>
       <nav className='container'>
         <ul className='flex items-center justify-between gap-8 font-medium tracking-wider text-stone-500'>
+          <Image src={logo} alt='Simply Put logo' height={96} />
           <li className='text-sm'>
             <Link href='/'>Home</Link>
           </li>
@@ -21,7 +24,7 @@ const Header = () => {
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
